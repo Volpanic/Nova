f387c5e04bb346868bff8b8903586a8624047b23
Volpanic <volpanicstudios@gmail.com>
2020-09-16 15:21:56 +1000

    Updated Design doc, Added music playing (Not currently in web build.)

---
+++ Nova/Assets/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
