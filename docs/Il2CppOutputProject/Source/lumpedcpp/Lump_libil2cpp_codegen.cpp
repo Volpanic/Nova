252731ed857e5fa0bdf67d9a1c37a9b5b2bf1082
Volpanic <volpanicstudios@gmail.com>
2020-09-18 14:45:21 +1000

    Made the web build dramatically smaller.

--- docs/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,3 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\s200491\Nova\Nova\Builds\Web\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "C:\Users\s200491\Nova\Nova\Builds\Web\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
